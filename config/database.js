25b9406f7c181f43d9056931ba003ba47c41dd75
NIck Ho <mahcyaa@gmail.com>
2024-03-24 22:57:22 +0800

    update config/database.js

--- config/database.js
+++ config/database.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
@@ -8,16 +9,8 @@ module.exports = ({ env }) => ({
       user: env("DATABASE_USERNAME", ""),
       password: env("DATABASE_PASSWORD", ""),
       ssl: env.bool('DATABASE_SSL', false) && {
-        key: env('DATABASE_SSL_KEY', undefined),
-        cert: env('DATABASE_SSL_CERT', undefined),
-        ca: env('DATABASE_SSL_CA', undefined),
-        capath: env('DATABASE_SSL_CAPATH', undefined),
-        cipher: env('DATABASE_SSL_CIPHER', undefined),
-        rejectUnauthorized: env.bool(
-          'DATABASE_SSL_REJECT_UNAUTHORIZED',
-          true
-        ),
-      },
+	      ca: env('DATABASE_SSL_CA', undefined),
+       },
     },
     useNullAsDefault: true,
   },
